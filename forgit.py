ec79ebb68ae372911f3615bb17f098acd30de974
Handle <handlefra@gmail.com>
2024-11-18 17:30:40 +0100

    Update forgit.py

--- forgit.py
+++ forgit.py
@@ -1,7 +1,7 @@
 import requests
 
 API_BASE = "https://fortnite-api.com/v2"
-API_KEY = "366fa857-3cef-4657-b011-4293a8adf656"  # Remplacez par votre clé d'API depuis le site Fortnite-API
+API_KEY = ""  # Remplacez par votre clé d'API depuis le site Fortnite-API
 
 def get_player_stats(player_name):
     url = f"{API_BASE}/stats/br/v2?name={player_name}"
